cadcdb656dd51df8e964390848f4979d89cbf106
Jesstern Rays <jsstrn@users.noreply.github.com>
2026-03-02 18:17:34 +0800

    Initial commit

---
+++ tests/test_variables.py
@@ -0,0 +1,7 @@
+from exercises.variables import age, name
+
+# DO NOT MODIFY THIS FILE
+
+def test_variables():
+  assert age == 25, "The age variable should be 25."
+  assert name == "Alex", "The name variable should be 'Alex'."
